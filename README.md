94e944c3db69c2b49f0a1d026e4689f3737cadb6
kimmi1206 <kheinerimm1206@gmail.com>
2022-08-09 17:32:42 -0500

    angular-front-passgen

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AngularPassgenApp
 
+# [https://angular-passgen-app.vercel.app/](https://angular-passgen-app.vercel.app/)
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.1.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,69 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular_passgen_app';
+  password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  // value.match(/^[0-9.,]+$/)
+  onChangeLength(eventTarget: EventTarget | null) {
+    const value = (eventTarget as HTMLInputElement).value;
+    const isValidValue = value.match(/^[0-9]+$/);
+    const parsedValue = isValidValue ? parseInt(value) : 0;
+    this.length = parsedValue <= 4096 ? parsedValue : 4096;
+    // this.length = !isNaN(parsedValue) ? parsedValue : 0;
+    // console.log(this.length);
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+    // console.log("Letters", this.includeLetters);
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+    // console.log("Numbers", this.includeNumbers);
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+    // console.log("Symbols", this.includeSymbols);
+  }
+
+  // getPassword() : string {
+  //   return this.password;
+  // }
+
+  // public get getPassword() : string {
+  //   return this.password;
+  // }
 }
